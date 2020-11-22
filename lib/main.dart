cee8b60e881888b6cc5bd39d45adcaadfd9ef553
abdelrahmanmahgoub <abdelrahman.rabie10@gmail.com>
2020-11-22 22:05:33 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
 
-
+-
 
 
         StreamProvider<MyUserProfile>.value(value: Global.usersRef.documentStream,),
